bc59d5150a7617da5ea59471c4a665ba94c0aeb8
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-02-23 12:02:56 -0500

    Build new message form

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,19 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Mini Message Board', messages });
 });
 
+// new message form
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+
+  res.redirect('/');
+});
+
 module.exports = router;
